5de810f509c88874b8682a3f5fe5dcd5a20b6ecb
KarlintheMiddle <47916114+KarlintheMiddle@users.noreply.github.com>
2021-07-25 23:29:13 +0800

    4. TransferFrom function added

--- test/LunaSphere.js
+++ test/LunaSphere.js
@@ -83,4 +83,45 @@ contract('LunaSphere', function (accounts) {
       assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer')
     })
   })
+
+  it('handles delegated token transfers', function(){
+    return LunaSphere.deployed().then(function(instance){
+      tokenInstance = instance;
+      fromAccount = accounts[2];
+      toAccount = accounts[3];
+      spendingAccount = accounts[4];
+      //transfers some token to fromAccount
+      return tokenInstance.transfer(fromAccount, 100, {from:accounts[0]});
+    }).then(function(receipt){
+      //Approve spendingAccount to spend 10 tokens from fromAccount
+      return tokenInstance.approve(spendingAccount,10,{from: fromAccount});
+    }).then(function(receipt){
+      return tokenInstance.transferFrom(fromAccount, toAccount, 9999, {from: spendingAccount});
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.toString().indexOf('revert') >= 0, 'Cannot transfer larger than balance');
+      //try transfering larger than the approve amount
+      return tokenInstance.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount});
+    }).then(assert.fail).catch(function(error){
+      assert(error.message.toString().indexOf('revert') >= 0, 'Cannot transfer larger than the approve amount');
+      return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {from: spendingAccount});
+    }).then(function(success){
+      assert.equal(success, true)
+      return tokenInstance.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount});
+    }).then(function(receipt){
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'Should be the Tranfer event');
+      assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
+      assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are tranferred to');
+      assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+      return tokenInstance.balanceOf(fromAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 90, 'deducts the amount from the sending account');
+      return tokenInstance.balanceOf(toAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 10, 'adds the amount from the receiving account')
+      return tokenInstance.allowance(fromAccount, spendingAccount);
+    }).then(function(allowance){
+      assert.equal(allowance, 0, 'deducts the amount from the allowance');
+    })
+  })
 });
