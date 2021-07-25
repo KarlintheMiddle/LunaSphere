8f4b0d8b20f93aad000386d63b9771c985e845ed
KarlintheMiddle <47916114+KarlintheMiddle@users.noreply.github.com>
2021-07-25 19:51:07 +0800

    2. Transfer added

--- test/LunaSphere.js
+++ test/LunaSphere.js
@@ -1,6 +1,22 @@
 var LunaSphere = artifacts.require('./LunaSphere.sol')
 
 contract('LunaSphere', function (accounts) {
+  
+  it('initialize the contract with the correct values', function() {
+    return LunaSphere.deployed().then(function(instance){
+      tokenInstance = instance;
+      return tokenInstance.name();
+    }).then(function(name) {
+      assert.equal(name,'LunaSphere','has the correct name');
+      return tokenInstance.symbol();
+    }).then(function(symbol){
+      assert.equal(symbol,'LNS','has the correct symbol');
+      return tokenInstance.standard();
+    }).then(function(standard){
+      assert.equal(standard,'Luna Sphere v1.0', 'has the correct standard')
+    })
+  });
+
   it('set the total supply upon deployment', function () {
     return LunaSphere.deployed()
       .then(function (instance) {
@@ -13,6 +29,39 @@ contract('LunaSphere', function (accounts) {
           1000000,
           'sets the total supply to 1000000',
         )
+        return tokenInstance.balanceOf(accounts[0])
       })
+      .then(function (adminBalance) {
+        assert.equal(
+          adminBalance.toNumber(),
+          1000000,
+          'allocate the initial supply to admin balance of 1000000',
+        )
+      })
+  });
+
+  it('transfers token ownership', function() {
+    return LunaSphere.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.transfer.call(accounts[1],9999999999999);
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+      return tokenInstance.transfer.call(accounts[1],250000, {from: accounts[0]});
+    }).then(function(success) {
+      assert.equal(success, true, 'it returns true');
+      return tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]});    
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'Should be the Tranfer event');
+      assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+      assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are tranferred to');
+      assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+      return tokenInstance.balanceOf(accounts[1]);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 250000, 'adds the ammount to the receiving account')
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 750000, 'deducts the amount to the sending account')
+    })
   })
 })
